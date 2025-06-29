8e2f5c1e0572f2a47888955f65f4dc2f23a3ed1d
Rieranthony <rieraanthony13@gmail.com>
2025-06-29 10:39:48 +0200

    feat: fix design

--- apps/web/src/components/copy-button.tsx
+++ apps/web/src/components/copy-button.tsx
@@ -1,60 +1,62 @@
 "use client";
 
-import { CheckIcon, ClipboardIcon } from "lucide-react";
 import * as React from "react";
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
-import {
-	Tooltip,
-	TooltipContent,
-	TooltipTrigger,
-} from "@/components/ui/tooltip";
+import { TooltipOnHover } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
+import Icon from "./ui/icons";
 
 export function copyToClipboardWithMeta(value: string) {
-	navigator.clipboard.writeText(value);
+  navigator.clipboard.writeText(value);
 }
 
 export function CopyButton({
-	value,
-	className,
-	variant = "ghost",
-	...props
+  value,
+  className,
+  variant = "ghost",
+  ...props
 }: React.ComponentProps<typeof Button> & {
-	value: string;
-	src?: string;
+  value: string;
+  src?: string;
 }) {
-	const [hasCopied, setHasCopied] = React.useState(false);
+  const [hasCopied, setHasCopied] = React.useState(false);
 
-	React.useEffect(() => {
-		setTimeout(() => {
-			setHasCopied(false);
-		}, 2000);
-	}, []);
+  React.useEffect(() => {
+    if (hasCopied) {
+      toast.success("Copied to clipboard", {
+        duration: 3000,
+      });
 
-	return (
-		<Tooltip>
-			<TooltipTrigger asChild>
-				<Button
-					className={cn(
-						"absolute top-3 right-2 z-10 size-7 bg-code hover:opacity-100 focus-visible:opacity-100",
-						className
-					)}
-					data-slot="copy-button"
-					onClick={() => {
-						copyToClipboardWithMeta(value);
-						setHasCopied(true);
-					}}
-					size="icon"
-					variant={variant}
-					{...props}
-				>
-					<span className="sr-only">Copy</span>
-					{hasCopied ? <CheckIcon /> : <ClipboardIcon />}
-				</Button>
-			</TooltipTrigger>
-			<TooltipContent>
-				{hasCopied ? "Copied" : "Copy to Clipboard"}
-			</TooltipContent>
-		</Tooltip>
-	);
+      setTimeout(() => {
+        setHasCopied(false);
+      }, 3000);
+    }
+  }, [hasCopied]);
+
+  return (
+    <TooltipOnHover content={hasCopied ? "Copied" : "Copy to Clipboard"}>
+      <Button
+        className={cn(
+          "absolute top-3 right-1 z-10 size-7 hover:opacity-100 focus-visible:opacity-100",
+          className
+        )}
+        data-slot="copy-button"
+        onClick={() => {
+          copyToClipboardWithMeta(value);
+          setHasCopied(true);
+        }}
+        size="icon"
+        variant={variant}
+        {...props}
+      >
+        <span className="sr-only">Copy</span>
+        {hasCopied ? (
+          <Icon name="check" />
+        ) : (
+          <Icon filledOnHover name="clipboard" />
+        )}
+      </Button>
+    </TooltipOnHover>
+  );
 }
--- apps/web/src/components/ui/input.tsx
+++ apps/web/src/components/ui/input.tsx
@@ -3,36 +3,36 @@ import type * as React from "react";
 import { cn } from "@/lib/utils";
 
 interface InputProps extends React.ComponentProps<"input"> {
-	prepend?: React.ReactNode;
-	append?: React.ReactNode;
+  prepend?: React.ReactNode;
+  append?: React.ReactNode;
 }
 
 function Input({ className, type, prepend, append, ...props }: InputProps) {
-	return (
-		<div className="relative flex w-full items-center">
-			{prepend && (
-				<div className="pointer-events-none absolute left-3 flex items-center">
-					{prepend}
-				</div>
-			)}
-			<input
-				className={cn(
-					"flex h-9 w-full min-w-0 rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-xs outline-none transition-[color,box-shadow] selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:bg-input/30",
-					"focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50",
-					"aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
-					prepend && "pl-10",
-					append && "pr-10",
-					className
-				)}
-				data-slot="input"
-				type={type}
-				{...props}
-			/>
-			{append && (
-				<div className="absolute right-3 flex items-center">{append}</div>
-			)}
-		</div>
-	);
+  return (
+    <div className="relative flex w-full items-center">
+      {prepend && (
+        <div className="pointer-events-none absolute left-3 flex items-center">
+          {prepend}
+        </div>
+      )}
+      <input
+        className={cn(
+          "flex h-9 w-full min-w-0 rounded-lg border border-input bg-transparent px-3 py-1 text-md shadow-xs outline-none transition-[color,box-shadow] selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:bg-input/30",
+          "focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50",
+          "aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
+          prepend && "pl-10",
+          append && "pr-10",
+          className
+        )}
+        data-slot="input"
+        type={type}
+        {...props}
+      />
+      {append && (
+        <div className="absolute right-3 flex items-center">{append}</div>
+      )}
+    </div>
+  );
 }
 
 export { Input };
