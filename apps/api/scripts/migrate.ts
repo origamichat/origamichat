c5d6be8107ebbb98d1465d7c6afec27ed76e707b
Rieranthony <rieraanthony13@gmail.com>
2025-07-10 14:42:51 +0200

    feat: migrate from Neon to PlanetScale *chef kiss*

--- apps/api/scripts/migrate.ts
+++ apps/api/scripts/migrate.ts
@@ -1,20 +1,18 @@
-import "dotenv/config";
-
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 import { db } from "../src/db";
 
 const main = async () => {
-	try {
-		await migrate(db, {
-			migrationsFolder: `${__dirname}/../drizzle/migrations`,
-		});
+  try {
+    await migrate(db, {
+      migrationsFolder: `${__dirname}/../drizzle/migrations`,
+    });
 
-		console.log("Migration completed");
-		process.exit(0);
-	} catch (error) {
-		console.error("Error during migration:", error);
-		process.exit(1);
-	}
+    console.log("Migration completed");
+    process.exit(0);
+  } catch (error) {
+    console.error("Error during migration:", error);
+    process.exit(1);
+  }
 };
 
 // biome-ignore lint/complexity/noVoid: false positive
