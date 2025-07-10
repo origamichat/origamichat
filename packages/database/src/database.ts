adfed10c6f066b530be4c9f504543d2aaf65d3f0
Rieranthony <rieraanthony13@gmail.com>
2025-07-10 11:27:10 +0200

    feat: switch from Neon to PlanetScale

--- packages/database/src/database.ts
+++ packages/database/src/database.ts
@@ -6,42 +6,42 @@ import { drizzle, type NodePgDatabase } from "drizzle-orm/node-postgres";
 import type * as schema from "./schema";
 
 const getEnvVariable = (name: string): string => {
-  const value = process.env[name];
+	const value = process.env[name];
 
-  if (value == null) {
-    console.error(`Environment variable ${name} not found`);
-    throw new Error(`environment variable ${name} not found`);
-  }
-  return value;
+	if (value == null) {
+		console.error(`Environment variable ${name} not found`);
+		throw new Error(`environment variable ${name} not found`);
+	}
+	return value;
 };
 
 let _db: NodePgDatabase<typeof schema> | null = null;
 
 const createDb = (): NodePgDatabase<typeof schema> => {
-  if (_db) {
-    return _db;
-  }
+	if (_db) {
+		return _db;
+	}
 
-  _db = drizzle({
-    connection: {
-      connectionString: getEnvVariable("DATABASE_URL"),
-      ssl: getEnvVariable("NODE_ENV") === "production",
-    },
-    cache: upstashCache({
-      url: getEnvVariable("UPSTASH_REDIS_REST_URL"),
-      token: getEnvVariable("UPSTASH_REDIS_REST_TOKEN"),
-      config: { ex: 60 },
-    }),
-  });
+	_db = drizzle({
+		connection: {
+			connectionString: getEnvVariable("DATABASE_URL"),
+			ssl: getEnvVariable("NODE_ENV") === "production",
+		},
+		cache: upstashCache({
+			url: getEnvVariable("UPSTASH_REDIS_REST_URL"),
+			token: getEnvVariable("UPSTASH_REDIS_REST_TOKEN"),
+			config: { ex: 60 },
+		}),
+	});
 
-  return _db;
+	return _db;
 };
 
 export type Database = ReturnType<typeof drizzle<typeof schema>>;
 
 export const db = new Proxy({} as Database, {
-  get: (target, prop) => {
-    const actualDb = createDb();
-    return actualDb[prop as keyof typeof actualDb];
-  },
+	get: (target, prop) => {
+		const actualDb = createDb();
+		return actualDb[prop as keyof typeof actualDb];
+	},
 });
--- packages/database/src/schema/waiting-list.ts
+++ packages/database/src/schema/waiting-list.ts
@@ -1,10 +1,10 @@
 import { relations } from "drizzle-orm";
 import {
-  boolean,
-  integer,
-  pgTable,
-  text,
-  timestamp,
+	boolean,
+	integer,
+	pgTable,
+	text,
+	timestamp,
 } from "drizzle-orm/pg-core";
 
 import { ulidPrimaryKey, ulidReference } from "../utils/ids";
@@ -12,25 +12,25 @@ import { ulidPrimaryKey, ulidReference } from "../utils/ids";
 import { user } from "./auth";
 
 export const waitingListEntry = pgTable("waiting_list_entry", {
-  id: ulidPrimaryKey("id"),
-  userId: ulidReference("user_id").references(() => user.id, {
-    onDelete: "cascade",
-  }),
-  fromReferralCode: text("from_referral_code"),
-  accessGranted: boolean("access_granted").notNull().default(false),
-  uniqueReferralCode: text("unique_referral_code").notNull().unique(),
-  points: integer("points").notNull().default(1),
-  createdAt: timestamp("created_at")
-    .$defaultFn(() => new Date())
-    .notNull(),
+	id: ulidPrimaryKey("id"),
+	userId: ulidReference("user_id").references(() => user.id, {
+		onDelete: "cascade",
+	}),
+	fromReferralCode: text("from_referral_code"),
+	accessGranted: boolean("access_granted").notNull().default(false),
+	uniqueReferralCode: text("unique_referral_code").notNull().unique(),
+	points: integer("points").notNull().default(1),
+	createdAt: timestamp("created_at")
+		.$defaultFn(() => new Date())
+		.notNull(),
 });
 
 export const waitingListEntryRelations = relations(
-  waitingListEntry,
-  ({ one }) => ({
-    user: one(user, {
-      fields: [waitingListEntry.userId],
-      references: [user.id],
-    }),
-  })
+	waitingListEntry,
+	({ one }) => ({
+		user: one(user, {
+			fields: [waitingListEntry.userId],
+			references: [user.id],
+		}),
+	})
 );
