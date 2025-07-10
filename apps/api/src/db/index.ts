967ffe20b02b9e7a814326b0f8b836ce5f0d0e63
Rieranthony <rieraanthony13@gmail.com>
2025-07-10 18:36:54 +0200

    feat: switch from Neon to PlanetScale

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -6,35 +6,35 @@ import type * as schema from "./schema";
 let _db: NodePgDatabase<typeof schema> | null = null;
 
 const createDb = (): NodePgDatabase<typeof schema> => {
-  if (_db) {
-    return _db;
-  }
+	if (_db) {
+		return _db;
+	}
 
-  _db = drizzle({
-    connection: {
-      host: env.DATABASE_HOST,
-      port: env.DATABASE_PORT,
-      user: env.DATABASE_USERNAME,
-      password: env.DATABASE_PASSWORD,
-      database: env.DATABASE_NAME,
-      ssl:
-        env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
-    },
-    cache: upstashCache({
-      url: env.UPSTASH_REDIS_REST_URL,
-      token: env.UPSTASH_REDIS_REST_TOKEN,
-      config: { ex: 60 },
-    }),
-  });
+	_db = drizzle({
+		connection: {
+			host: env.DATABASE_HOST,
+			port: env.DATABASE_PORT,
+			user: env.DATABASE_USERNAME,
+			password: env.DATABASE_PASSWORD,
+			database: env.DATABASE_NAME,
+			ssl:
+				env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+		},
+		cache: upstashCache({
+			url: env.UPSTASH_REDIS_REST_URL,
+			token: env.UPSTASH_REDIS_REST_TOKEN,
+			config: { ex: 60 },
+		}),
+	});
 
-  return _db;
+	return _db;
 };
 
 export type Database = ReturnType<typeof drizzle<typeof schema>>;
 
 export const db = new Proxy({} as Database, {
-  get: (target, prop) => {
-    const actualDb = createDb();
-    return actualDb[prop as keyof typeof actualDb];
-  },
+	get: (target, prop) => {
+		const actualDb = createDb();
+		return actualDb[prop as keyof typeof actualDb];
+	},
 });
