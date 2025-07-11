6b3ddc470da2a462c9a485294693652d176dfa71
Rieranthony <rieraanthony13@gmail.com>
2025-07-11 19:19:26 +0200

    feat: remove dark mode

--- apps/web/src/app/(lander-docs)/layout.tsx
+++ apps/web/src/app/(lander-docs)/layout.tsx
@@ -1,23 +1,23 @@
 import { Suspense } from "react";
 import {
-  DashboardButton,
-  DashboardButtonSkeleton,
+	DashboardButton,
+	DashboardButtonSkeleton,
 } from "@/app/(lander-docs)/components/topbar/dashboard-button";
 import { Footer } from "./components/footer";
 import { TopBar } from "./components/topbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="relative flex min-h-svh flex-col overflow-clip border-grid-x">
-      <TopBar>
-        <Suspense fallback={<DashboardButtonSkeleton />}>
-          <DashboardButton />
-        </Suspense>
-      </TopBar>
-      <main className="flex flex-1 flex-col">
-        <div className="container-wrapper mx-auto">{children}</div>
-      </main>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="relative flex min-h-svh flex-col overflow-clip border-grid-x">
+			<TopBar>
+				<Suspense fallback={<DashboardButtonSkeleton />}>
+					<DashboardButton />
+				</Suspense>
+			</TopBar>
+			<main className="flex flex-1 flex-col">
+				<div className="container-wrapper mx-auto">{children}</div>
+			</main>
+			<Footer />
+		</div>
+	);
 }
